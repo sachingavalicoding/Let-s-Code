92ed5fb9cf692febcfdfe92e048ef154d6079492
sachingavalicoding <sachingavali321@gmail.com>
2026-08-26 22:23:33 +0530

    login system Folder structure

---
+++ login_system/client/src/App.jsx
@@ -0,0 +1,10 @@
+import './index.css'
+import Signup from './components/Signup';
+
+const App = () => {
+  return <div className="flex text-center text-blue-400">
+    App
+  </div>
+}
+
+export default App;
---
+++ login_system/client/src/components/Signup.jsx
@@ -0,0 +1,7 @@
+const Signup = () => {
+    return (<div>
+        <h1> Sign up Page</h1>
+    </div>)
+}
+
+export default Signup;
