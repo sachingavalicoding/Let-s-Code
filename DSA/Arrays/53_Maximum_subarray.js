e1a21c4ecc26bcd212ebec38a4c6455deabc32ef
sachingavalicoding <sachingavali321@gmail.com>
2026-08-27 21:14:44 +0530

    Maximum subarray optimize

--- DSA/Arrays/53_Maximum_subarray.js
+++ DSA/Arrays/53_Maximum_subarray.js
@@ -1,5 +1,6 @@
 const nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
 
+// O(n^2)
 const maximumSubArraySum = (nums) => {
     if (!nums || nums?.length === 0) return [];
     let max = 0;
@@ -16,5 +17,18 @@ const maximumSubArraySum = (nums) => {
     return max;
 }
 
+// O(n)
+
+const subArraySum = (arr) => {
+    let res = arr[0]; // 
+    let maxEnding = arr[0];
+
+    for (let i = 1; i < arr.length; i++) {
+        maxEnding = Math.max(maxEnding + arr[i], arr[i]);
+        res = Math.max(res, maxEnding);
+    }
+    return res;
+}
 let ans = maximumSubArraySum(nums);
-console.log(ans);
+let ans2 = subArraySum(nums);
+console.log(ans2);
